4dfe0a3b9a42c7221c10fc3c9d6e1d96d2526d5d
Anton <sparco114@ya.ru>
2023-01-05 02:47:33 +0530

    done - 00_distance.py

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Есть словарь координат городов
-
+from pprint import pprint
 sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
@@ -14,10 +14,29 @@ sites = {
 
 distances = {}
 
-# TODO здесь заполнение словаря
+mskX = sites['Moscow'][0]
+lonX = sites['London'][0]
+parX = sites['Paris'][0]
+mskY = sites['Moscow'][1]
+lonY = sites['London'][1]
+parY = sites['Paris'][1]
 
-print(distances)
+msk_lon = ((mskX - lonX) ** 2 + (mskY - lonY) ** 2) ** 0.5
+msk_par = ((mskX - parX) ** 2 + (mskY - parY) ** 2) ** 0.5
 
+distances['MOSCOW'] = {}
+distances['MOSCOW']['TO LONDON'] = msk_lon
+distances['MOSCOW']['TO PARIS'] = msk_par
 
+lon_par = ((lonX - parX) ** 2 + (lonY - parY) ** 2) ** .5
 
+distances['LONDON'] = {}
+distances['LONDON']['TO MOSCOW'] = msk_lon
+distances['LONDON']['TO PARIS'] = lon_par
+
+distances['PARIS'] = {}
+distances['PARIS']['TO MOSCOW'] = msk_par
+distances['PARIS']['TO LONDON'] = lon_par
+
+pprint(distances)
 

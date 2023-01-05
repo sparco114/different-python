ce73891450769f0c543e9028809d742476460631
Anton <sparco114@ya.ru>
2023-01-05 11:17:41 +0530

    done - 4/10 - 04_my_family.py

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -5,16 +5,25 @@
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
 my_family = []
+my_family.append('I')
+my_family.extend(['fother', 'grandpa', 'jon'])
+my_family.insert(0, 'mother')
 
+print(my_family)
 
 # список списков приблизителного роста членов вашей семьи
-my_family_height = [
-    # ['имя', рост],
-    [],
-]
+my_family_height = [['I', 172], ['fother', 178]]
 
+my_family_height.append(['grandpa', 166])
+my_family_height.insert(0, ['mother', 168])
+my_family_height.extend([['jon', 179]])
+
+# print(my_family_height)
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
 
+print(f'Рост отца - {my_family_height[2][1]} см')
+
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+print(f'Общий рост моей семьи - {(my_family_height[0][1]) + (my_family_height[1][1]) + (my_family_height)[2][1] + (my_family_height[3][1]) + (my_family_height[4][1])} см')

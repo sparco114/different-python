e2aa86f13aa688f5b222dc828b2c819bf53a42cc
anton <sparco114@ya.ru>
2023-04-09 22:25:24 +0530

    done - man_and_cats - lesson_007

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,7 +19,52 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+class Water:
+
+    def __str__(self):
+        return 'Вода'
+
+    def __add__(self, other):
+        # print(type(Air()), type(other), 'Воздух', other == Air())
+        if isinstance(other, type(Air())):
+            return Storm()
+        elif other.__str__() == 'Огонь':
+            return 'Пар'
+        else:
+            return None
+
+
+class Fire:
+
+    def __str__(self):
+        return 'Огонь'
+
+    def __add__(self, other):
+        if other.__str__() == 'Воздух':
+            return 'Молния'
+        elif other.__str__() == 'Вода':
+            return 'Пар'
+
+
+class Air:
+
+    def __str__(self):
+        return 'Воздух'
+
+
+class Storm:
+
+    def __str__(self):
+        return 'Шторм'
+
+
+# a = Air()
+# w = Water()
+
+# print(Air())
+print(Water(), '+', Air(), '=', Water() + Air())
+print(Fire(), '+', Air(), '=', Fire() + Air())
+print(Fire(), '+', Water(), '=', Fire() + Water())
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
--- lesson_007/03_man_ans_cat.py
+++ lesson_007/03_man_ans_cat.py
@@ -25,7 +25,164 @@ from random import randint
 
 # Человеку и коту надо вместе прожить 365 дней.
 
-# TODO здесь ваш код
+from random import randint
+from termcolor import cprint, colored
+
+
+class Man:
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+
+    def __str__(self):
+        return f"Я - {self.name}, сытость: {self.fullness}"
+
+    def eat(self):
+        if self.house.food >= 10:
+            print(f'{self.name} поел')
+            self.fullness += 30
+            self.house.food -= 10
+        else:
+            self.fullness -= 10
+            cprint(f'У {self.name} нет еды', 'yellow')
+
+    def work(self):
+        print(f'{self.name} cходил на работу')
+        self.house.money += 150
+        self.fullness -= 20
+
+    def watching_tv(self):
+        print(f'{self.name} смотрел МТВ')
+        self.fullness -= 10
+
+    def shopping(self):
+        if self.house.money >= 50:
+            print(f'{self.name} купил еды')
+            self.house.money -= 50
+            self.house.food += 50
+        else:
+            cprint(f'У {self.name} деньги кончились', 'yellow')
+
+    def go_into_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        print(f'{self.name} заехал в дом')
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint(f'{self.name} умер от голода', 'red')
+            return
+        dice = randint(1, 6)
+        if self.fullness <= 20:
+            self.eat()
+        elif self.house.food <= 10:
+            self.shopping()
+        elif self.house.money < 50:
+            self.work()
+        elif self.house.cat_food <= 20:
+            self.shopping_cat_food()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.watching_tv()
+
+    def take_cat(self, cat):
+        cat.house = self.house
+        print(f'{self.name} взял кота в дом {self.house.house_name}')
+
+    def shopping_cat_food(self):
+        print(f'{self.name} купил коту еды')
+        self.house.money -= 50
+        self.house.cat_food += 50
+
+    def cleaning(self):
+        print(f'{self.name} прибрался в доме')
+        self.fullness -= 20
+        self.house.dirt += 100
+
+
+class House:
+
+    def __init__(self, house_name):
+        self.food = 10
+        self.money = 100
+        self.house_name = house_name
+        self.cat_food = 0
+        self.dirt = 0
+
+    def __str__(self):
+        return f'В доме осталось еды: {self.food}, кошачьей еды: {self.cat_food}, денег: {self.money}'
+
+
+class Cat:
+
+    def __init__(self, cat_name):
+        self.house = None
+        self.cat_name = cat_name
+        self.cat_fullness = 30
+
+    def __str__(self):
+        return f'Кот {self.cat_name}, в доме {self.house.house_name}. Сытость: {self.cat_fullness}'
+
+    def sleep(self):
+        print(f'Кот {self.cat_name} спит')
+        self.cat_fullness -= 10
+
+    def eat(self):
+        if self.house.cat_food >= 10:
+            print(f'Кот {self.cat_name} поел')
+            self.cat_fullness += 20
+            self.house.cat_food -= 10
+        else:
+            print('Нет кошачьей еды')
+            self.cat_fullness -= 10
+
+    def dirting(self):
+        print(f'Кот {self.cat_name} дерет обои')
+        self.cat_fullness -= 10
+        self.house.dirt -= 5
+
+    def act(self):
+        if self.cat_fullness <= 0:
+            cprint(f'Кот {self.cat_name} умер от голода', 'red')
+            return
+        dice = randint(1, 2)
+        if self.cat_fullness <= 20:
+            self.eat()
+        elif dice == 1:
+            self.sleep()
+        else:
+            self.dirting()
+
+
+beavis = Man('Бивис')
+my_sweet_home = House('sweet_home')
+beavis.go_into_house(my_sweet_home)
+mark = Cat('Марк')
+vaska = Cat('Васька')
+murka = Cat('Мурка')
+
+beavis.take_cat(mark)
+beavis.take_cat(vaska)
+beavis.take_cat(murka)
+
+for day in range(1, 366):
+    cprint(f'===================== DAY {day} =====================', 'blue')
+    beavis.act()
+    mark.act()
+    vaska.act()
+    murka.act()
+    print('Итог дня:')
+    print(beavis)
+    print(mark)
+    print(vaska)
+    print(murka)
+    print(my_sweet_home)
+
 
 # Усложненное задание (делать по желанию)
 # Создать несколько (2-3) котов и подселить их в дом к человеку.

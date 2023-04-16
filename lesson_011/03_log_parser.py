313ebeb1ac47634c4d4c543f0c476055016fcc0d
anton <sparco114@ya.ru>
2023-04-17 03:10:50 +0530

    done - lesson 011 - 03_log_parser.py +++

--- lesson_011/03_log_parser.py
+++ lesson_011/03_log_parser.py
@@ -14,4 +14,26 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+def parse_nok():
+    res = {}
+    with open('events-.txt', mode='r') as events:
+        prev_date = None
+        for line in events:
+            date = line[1:17]
+            if 'NOK' in line:
+                if date == prev_date:
+                    res[date] += 1
+                else:
+                    if prev_date:
+                        yield prev_date, res[prev_date]
+                    res[date] = 1
+                    prev_date = date
+        if prev_date:
+            yield prev_date, res[prev_date]
+
+
+grouped_events = parse_nok()
+for group_time, event_count in grouped_events:
+    print(f'[{group_time}] {event_count}')
+    # with open('result.txt', mode='a') as ff:
+    #     ff.write(f'{group_time} {event_count}\n')
--- lesson_011/test.py
+++ lesson_011/test.py
@@ -1,47 +1,24 @@
-# def get_prime_numbers(n):
-#     prime_numbers = []
-#     for number in range(2, n + 1):
-#         for prime in prime_numbers:
-#             if number % prime == 0:
-#                 break
-#         else:
-#             prime_numbers.append(number)
-#     return prime_numbers
-#
-#
-# print(get_prime_numbers(100))
+def parse_nok():
+    res = {}
+    with open('events-.txt', mode='r') as events:
+        prev_date = None
+        for line in events:
+            date = line[1:17]
+            if 'NOK' in line:
+                if date == prev_date:
+                    res[date] += 1
+                else:
+                    if prev_date:
+                        yield prev_date, res[prev_date]
+                    res[date] = 1
+                    prev_date = date
+        if prev_date:
+            yield prev_date, res[prev_date]
 
 
-# -----------------------------------------------------------------------------
-
-
-def check(numberrr):
-    str_nu = str(numberrr)
-    len_nu = len(str_nu)
-    a = str_nu[:(int(len_nu / 2))]
-    b = ''.join(reversed(str_nu[int(-(len_nu / 2)):]))
-    if len_nu % 2 == 0:
-        if a == b:
-            return True
-    else:
-        if str_nu[:(int((len_nu - 1) / 2))] == ''.join(reversed(str_nu[int(-(len_nu / 2)):])):
-            return True
-    return False
-
-
-def prime_numbers_generator(n, func):
-    prime_numbers = []
-    for nu in range(2, n + 1):
-        for prime in prime_numbers:
-            if nu % prime == 0:
-                break
-        else:
-            if func(nu):
-                prime_numbers.append(nu)
-                yield nu
-    return
-
-
-for number in prime_numbers_generator(n=2000, func=check):
-    print(number)
+grouped_events = parse_nok()
+for group_time, event_count in grouped_events:
+    print(f'[{group_time}] {event_count}')
+    # with open('result.txt', mode='a') as ff:
+    #     ff.write(f'{group_time} {event_count}\n')
 
